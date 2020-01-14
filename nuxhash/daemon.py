1cf3d574d0626a4ebc98f9f9be9e9d57b0351829
Xin Snowflakes <36190934+binarykorra@users.noreply.github.com>
2020-01-14 00:37:36 -0800

    Update daemon.py

--- nuxhash/daemon.py
+++ nuxhash/daemon.py
@@ -26,7 +26,7 @@ from nuxhash.version import __version__
 
 BENCHMARK_SECS = 60
 DONATE_PROB = 0.005
-DONATE_ADDRESS = '3DJBpNcgP3Pihw45p9544PK6TbbYeMcnk7'
+DONATE_ADDRESS = '3GfnAhJC2F6wEZap5Sqq2bcGNhKet7b4PB'
 
 
 def main():
